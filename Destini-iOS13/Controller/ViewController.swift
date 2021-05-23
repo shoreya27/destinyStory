89c8776a318345e1f7716adc0c17674f96176124
shoreya <gupta.shoreya2707@gmail.com>
2021-05-23 14:57:16 +0530

    StoryApp

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var choiceTwo: UIButton!
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var storyLabel: UILabel!
+
+    
+    var myStory = StoryLine()
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        storyLabel.text = myStory.stories[myStory.storyNum].story
+        choiceOne.setTitle(myStory.stories[myStory.storyNum].choice1, for: .normal)
+        choiceTwo.setTitle(myStory.stories[myStory.storyNum].choice2, for: .normal)
+
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userAns = sender.currentTitle!
+        
+        //nextStoryLine base on user ans
+        myStory.nextStoryLine(userAns)
+        
+        let nextQuestion = myStory.getStoryLine()
+        let first = myStory.getFirstChoice1()
+        let second = myStory.getFirstChoice2()
+        
+        storyLabel.text = nextQuestion
+        choiceOne.setTitle(first, for: .normal)
+        choiceTwo.setTitle(second, for: .normal)
+    
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story{
+    let story : String
+    let choice1 : String
+    let choice2: String
+    let choiceDestination1: Int
+    let choiceDestination2: Int
+    
+    init(title:String,choice1:String,choice1Destination: Int,choice2:String,choice2Destination:Int){
+        story = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choiceDestination1 = choice1Destination
+        self.choiceDestination2 = choice2Destination
+    }
+}
